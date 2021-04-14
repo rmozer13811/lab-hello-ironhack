920798c0e942796407f3a5d49e61f2626555b7ea
Richard Carl Mozer <r_carl1997@hotmail.com>
2021-04-14 03:27:01 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
+    const App = () => {
+
+      const cards = [
+        {
+          img: '/images/icon1.png',
+          title: 'Declarative',
+          text: 'React makes It painless to create interactive UIs.',
+        },
+        {
+          img: '/images/icon2.png',
+          title: 'Components',
+          text: 'Build encapsulated components that manage their state.',
+        },
+        {
+          img: '/images/icon3.png',
+          title: 'Single-Way',
+          text: 'A set of immutable values are passed to the component’s.',
+        },
+        {
+          img: '/images/icon4.png',
+          title: 'JSX',
+          text: 'Statically-typed, designed to run on modern browsers.',
+        },
+      ];
+    
+      const drawCards = (array) => {
+        return array.map((card, idx) => {
+          return (
+          <div key={`element-${idx}`} className="card">
+            <img src={card.img} alt={`img-${idx + 1}`} / >
+            <h2>{card.title}</h2>
+            <p>{card.text}</p>
+          </div>
+          );
+        });
+      };
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Say hello to ReactJS</p>
+        <p className="">
+          You will learn how to use the most popular frontend library, and
+          become a super Ninja developer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div></div>
     </div>
   );
 }
